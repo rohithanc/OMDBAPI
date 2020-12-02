dd3608728f8998cbe95b285199c8c8581544c131
rohithanc <rohithan@msn.com>
2020-12-02 14:02:36 +0000

    Task upload

--- src/Components/DataRequests/FilmAbout.jsx
+++ src/Components/DataRequests/FilmAbout.jsx
@@ -6,6 +6,8 @@ const FilmAbout = () => {
         <>
             <p> Insert text here</p>
             <p> Information about the film</p>
+            <p> Some reviews and ratings</p>
+            <p> Lorem ipsum dolor sit amet </p>
         </>
     );
 
